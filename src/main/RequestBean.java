bb8bc24591afa4905cc40e24368fd6f91716509a
Marcin Gołda <marcon1993@gmail.com>
2016-11-21 19:07:27 +0100

    Dodanie dodatkowych elementow

--- src/main/RequestBean.java
+++ src/main/RequestBean.java
@@ -1,19 +1,39 @@
 package main;
 
+import java.util.ArrayList;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 @ManagedBean(name = "RequestBean")
 @RequestScoped
 public class RequestBean {	
-	private String requestTest;
+	private String sex;
+	private String favColor;
+	private ArrayList<String> favFilm;
 
-	public String getRequestTest() {
-		return requestTest;
+	public String getSex() {
+		return sex;
 	}
 
-	public void setRequestTest(String requestTest) {
-		this.requestTest = requestTest;
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	public String getFavColor() {
+		return favColor;
+	}
+
+	public void setFavColor(String favColor) {
+		this.favColor = favColor;
+	}
+
+	public ArrayList<String> getFavFilm() {
+		return favFilm;
+	}
+
+	public void setFavFilm(ArrayList<String> favFilm) {
+		this.favFilm = favFilm;
 	}
 
 }
